e33919d02c2daf9702bbc485cfdbd1f2d65dcae3
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2023-09-09 15:07:38 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# MY--PORTFOLIO-WEBSITE
-This is my portfolio website where you get to know about me and you will get my social links...
+# Portfolio Website (OLD)
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/d5243264-1e02-441b-95ee-b2b2adbbbea6/deploy-status)](https://app.netlify.com/sites/lohitkolluri-old/deploys)
+
+This is the documentation for my portfolio website, which showcases my projects and provides information about me. Now i use a new portfolio website.
+
+## File Structure
+
+The file structure of the website is as follows:
+
+```
+- composer.json
+- index.html
+- index.php
+- README.md
+- images/
+    - terminal.png
+- scripts/
+    - cursor.js
+    - data.js
+    - main.js
+- style/
+    - style.css
+```
+
+## Description
+
+This portfolio website serves as a platform for me to showcase my projects, provide information about myself, and display my social media profiles. It has a terminal-like interface that allows users to interact with the website using commands.
+
+## Usage
+
+To view the website, simply open the `index.html` file in a web browser. The website is designed to be responsive and compatible with modern browsers.
+
+## Dependencies
+
+The website does not have any external dependencies. However, it uses the following files for its functionality:
+
+- `cursor.js`: Handles the behavior of the cursor on the website.
+- `data.js`: Contains data used for various sections of the website, such as the about section, banner, help menu, etc.
+- `main.js`: Contains the main JavaScript code for the website.
+
+## Customization
+
+If you wish to customize the website, you can modify the following files:
+
+- `style.css`: Allows you to change the colors, styles, and layout of the website.
+- `data.js`: Modify the content in this file to update the about section, banner, help menu, and other sections of the website.
+
+Feel free to explore the website and enjoy!
---
+++ scripts/data.js
@@ -0,0 +1,86 @@
+const linkedin = "https://www.linkedin.com/in/tanishq-j-913152288/";
+const github = "https://github.com/TJ-2004";
+const email = "mailto:tanishqjais9756@gmail.com";
+var today = new Date();
+
+about = [
+  "<br>",
+  "Hey, I'm Tanishq Jaiswal! 👋",
+  "<br>",
+  "I'm a student at SRM University, Kattankulathur,.",
+  "I am currently pursuing B-TECH in CSE Department with specialization of Blockchain Technology.",
+  "I love to build things that are useful and can have a positive impact on other people's lives.",
+  "Having said that, I'm always ready to work on quirky projects",
+  "I am always ready to explore new things and collaborate with others.",
+  "Gathering a lot of knowledge and insights.",
+  "Outside of the professional space, I love playing games and casual streaming.",
+  "<br>",
+  "Hope you explore and enjoy this website! 🙏🏻",
+  "<br>",
+];
+
+banner = [
+  "<br>",
+  '<span class = "index">' + String(today) + "</span>",
+  '<span class="index">Tanishq Jaiswal </span>',
+  "<br>",
+  "T",
+  "A",
+  "I",
+  "S",
+  "H",
+  "Q",
+  "\n",
+  "J",
+  "A",
+  "I",
+  "S",
+  "W",
+  "A",
+  "L",
+  "<br>",
+  '<span class="color2">Welcome to my Portfolio Website..</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+  // '<span class="color2"></span>',
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '👦🏻  <span class="command">About</span>          About Me',
+  '🌏  <span class="command">Social</span>         Display social networks',
+  '🏗  <span class="command">Projects</span>       View coding projects',
+  '📧  <span class="command">Email</span>          Reach me through email',
+  '🧹  <span class="command">Clear</span>          Clear the terminal',
+  '❓  <span class="command">Help</span>           Open the help menu',
+  '🕰  <span class="command">History</span>        View command history',
+  '👋  <span class="command">Exit</span>           Close the tab and exit',
+  "<br>",
+];
+
+social = [
+  "<br>",
+  "Enter the name of any of the following or simply click the links...",
+  "<br>",
+  'Linkedin       <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/tanishq-j-913152288' +
+    "</a>",
+  'Github         <a href="' +
+    github +
+    '" target="_blank">github/TJ-2004' +
+    "</a>",
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  '<span class="command">1. WeatherPedia</span>',
+  "<br>",
+  "<br>",
+  '<span class="command">2. Mental Health Fitness Tracker',
+  "<br>",
+  "<br>",
+  '<span class="command">4. Portfolio Website (This Website)</span>',
+  "<br>",
+];
